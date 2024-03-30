f3a9d6356c82869dff39e066c5f2695147d97f9d
fernandojrdev <fernandomtcjunior@gmail.com>
2024-03-30 15:01:10 -0300

    update

--- assets/script/slick.js
+++ assets/script/slick.js
@@ -2,8 +2,24 @@ $('.carousel-portfolio').slick({
     dots: true,
     infinite: true,
     arrows: false,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
