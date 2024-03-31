979b485bd3722884c3970ae1622b6fb429182df8
fernandojrdev <fernandomtcjunior@gmail.com>
2024-03-31 19:43:46 -0300

    update

---
+++ assets/script/function.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("Sidenav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
